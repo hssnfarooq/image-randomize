abcb98b410c2bf1c376871fc3c1110c10098ec2c
hssnfarooq <hhhassanhafeez91@gmail.com>
2022-09-17 03:33:22 +0500

    image list changes

--- js/list.js
+++ js/list.js
@@ -1,16 +1,16 @@
-window.addEventListener('load', function () {
-  var list = document.getElementById('list');
-  fetch('data.json')
+window.addEventListener("load", function () {
+  var list = document.getElementById("list");
+  fetch("data.json")
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      var html = '';
+      var html = "";
       data.forEach(function (item) {
-        html += '<div class="random-image mar-t">';
+        html += '<div class="random-image-list mar-t">';
         html += "<img src='" + item.image + "' />";
-        html += '<p class="random-image-text">' + item.text + '</p>';
-        html += '</div>';
+        html += '<p class="random-image-text">' + item.text + "</p>";
+        html += "</div>";
       });
       list.innerHTML = html;
     });
